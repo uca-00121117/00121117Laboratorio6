615f611d19bcb97384534ec847cba5963ee506ea
Drubico <drubico@github.com>
2020-05-21 19:22:53 -0600

    Terminado?

--- code/00121117.cpp
+++ code/00121117.cpp
@@ -6,11 +6,17 @@
 #include "sel.h"
 #include "assembly.h"
 
+string print_00121117()
+{
+    return "\n\n\nMETODO DE LOS ELEMENTOS FINITOS EN DOS DIMENSIONES CON FUNCIONES DE FORMA LINEALES Y PESOS DE GALERKIN - 00121117 \n \n \n ";
+};
 
 int main(int argc, char *argv[])
 {
     char filename[150];
-    strcpy(filename,argv[1]);
+    strcpy(filename, argv[1]);
+
+    cout << print_00121117();
 
     vector<Matrix> localKs;
     vector<Vector> localbs;
@@ -18,30 +24,28 @@ int main(int argc, char *argv[])
     Vector b;
     Vector u;
 
-    cout << "METODO DE LOS ELEMENTOS FINITOS EN DOS DIMENSIONES CON FUNCIONES DE FORMA LINEALES Y PESOS DE GALERKIN - 00121117 \n ";
-
     mesh m;
-    leerMallayCondiciones(m,filename);
+    leerMallayCondiciones(m, filename);
     cout << "Datos obtenidos correctamente\n********************\n";
 
-    crearSistemasLocales(m,localKs,localbs);
+    crearSistemasLocales(m, localKs, localbs);
 
-    zeroes(K,m.getSize(NODES));
-    zeroes(b,m.getSize(NODES));
-    ensamblaje(m,localKs,localbs,K,b);
+    zeroes(K, m.getSize(NODES));
+    zeroes(b, m.getSize(NODES));
+    ensamblaje(m, localKs, localbs, K, b);
 
-    applyNeumann(m,b);
+    applyNeumann(m, b);
 
-    applyDirichlet(m,K,b);
+    applyDirichlet(m, K, b);
 
     showMatrix(K);
-    zeroes(u,b.size());
-    calculate(K,b,u);
+    zeroes(u, b.size());
+    calculate(K, b, u);
 
     cout << "La respuesta es: \n";
     showVector(u);
 
-    writeResults(m,u,filename);
+    writeResults(m, u, filename);
 
     return 0;
-}
+};
